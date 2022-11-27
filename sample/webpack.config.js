bda04af2340f56067c04eee66e892a0846f6a96a
seung-gumon <b9327912@gmail.com>
2022-11-27 21:39:16 +0900

    gitignore추가

---
+++ sample/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+
+console.log(path.resolve())
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/app.js'
+    },
+    output: {
+        path: path.resolve('./dist'),
+        filename: '[name].js'
+    }
+}
